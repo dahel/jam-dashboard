62f6542190b6c3f7369cd2d1d53e3f78930fc72d
Sebastian Prymas <prymas.sebastian@gmail.com>
2020-04-20 13:57:57 +0200

    initial commit

---
+++ src/components/profile.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+const Profile =() => {
+    return (
+        <div className="dashboard-header">
+            <nav>
+                <Link
+                    to="/dashboard/secret"
+                    activeClassName="active"
+                >
+                    Secret Stuff
+                </Link>
+                <Link
+                    to="/dashboard/base"
+                    activeClassName="active"
+                >
+                    See Your Base
+                </Link>
+            </nav>
+            <span>Todo show login status</span>
+        </div>
+    )
+}
+
+export default Profile;
